b48866df4486f5c53eb98482349eb9379e5e9a94
Maciej Sawicki <macieksawickim@gmail.com>
2023-05-21 20:29:15 +0200

    update

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ const navSlide = () => {
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
     const menuFunction = () => {
-    nav.classList.toggle("nav-active");
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = "";
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${
+        nav.classList.toggle("nav-active");
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${
             index / 7 + 0.3
             }s`;
-        }
-    });
-    burger.classList.toggle("toggle");
+            }
+        });
+        burger.classList.toggle("toggle");
     };
     burger.addEventListener("click", menuFunction);
     navLinks.forEach((link) => {
-    link.addEventListener("click", menuFunction);
+        link.addEventListener("click", menuFunction);
     });
 };
 
 const mql = window.matchMedia('(max-width: 950px)');
+
+
 
 function windowMatch(e) {
     if (e.matches) {
@@ -30,4 +32,3 @@ function windowMatch(e) {
 }
 
 mql.addEventListener('change', windowMatch);
-
